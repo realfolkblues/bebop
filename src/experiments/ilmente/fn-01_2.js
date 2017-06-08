30b91b9cd78e0a1b3913b5867627b71e9d2edfa0
Alessandro Bellini <alessandro@ilmente.it>
2017-06-08 19:39:41 +0200

    create a rudimental tree

--- src/experiments/ilmente/fn-01_2.js
+++ src/experiments/ilmente/fn-01_2.js
@@ -3,37 +3,72 @@ const path = require('path');
 const esprima = require('esprima');
 const Rx = require('rxjs');
 
+let globalTree = {};
 const deps = new Rx.Subject();
 
 const files = deps
-    .startWith('index')
     .map(dep => path.resolve('./examples/fn/01/', dep) + '.js');
 
-const nodes = files
-    .flatMap(file => Rx.Observable.bindNodeCallback(fs.readFile)(file, 'utf8'))
-    .flatMap(code => Rx.Observable.fromEventPattern(handler => esprima.parse(code, { sourceType: 'module' }, handler)));
+files.subscribe(file => { 
+    console.log('working on', file);
+    abc(file);
+}, error => { 
+    console.error(error);
+}, () => { 
+    console.log(123);
+}); 
 
-const imports = nodes
-    .filter(node => node.type === 'ImportDeclaration')
-    .map(node => node.source.value);
+function abc(filename) { 
+    let analysis = {
+        imports: [],
+        declaredFn: [],
+        invokedFn: [],
+        exportedFn: []
+    };
 
-const invokes = nodes
-    .filter(node => node.type === 'CallExpression' && node.callee.type === 'Identifier')
-    .map(node => node.callee.name);
+    globalTree[filename] = analysis;
 
-const declared = nodes
-    .filter(node => node.type === 'FunctionDeclaration')
-    .map(node => node.id.name);
+    const nodes = Rx.Observable
+        .from([filename])
+        .flatMap(file => Rx.Observable.bindNodeCallback(fs.readFile)(file, 'utf8'))
+        .flatMap(code => Rx.Observable.fromEventPattern(handler => esprima.parse(code, { sourceType: 'module' }, handler)));
 
-const exported = nodes
-    .filter(node => node.type === 'ExportNamedDeclaration')
-    .map(node => node.declaration.id.name);
+    const imports = nodes
+        .filter(node => node.type === 'ImportDeclaration')
+        .map(node => node.source.value);
 
-imports.subscribe(value => {
-    deps.next(value);
-    console.log('import from', value);
-});
+    const invokedFn = nodes
+        .filter(node => node.type === 'CallExpression' && node.callee.type === 'Identifier')
+        .map(node => node.callee.name);
 
-declared.subscribe(value => console.log('declare', value));
-invokes.subscribe(value => console.log('invoke', value));
-exported.subscribe(value => console.log('export', value));
+    const declaredFn = nodes
+        .filter(node => node.type === 'FunctionDeclaration')
+        .map(node => node.id.name);
+
+    const exportedFn = nodes
+        .filter(node => node.type === 'ExportNamedDeclaration')
+        .map(node => node.declaration.id.name);
+    
+    imports.subscribe(value => { 
+        globalTree[filename].imports.push(value);
+        deps.next(value);
+    });
+
+    declaredFn.subscribe(value => { 
+        globalTree[filename].declaredFn.push(value);
+    });
+
+    invokedFn.subscribe(value => { 
+        globalTree[filename].invokedFn.push(value);
+    });
+
+    exportedFn.subscribe(value => { 
+        globalTree[filename].exportedFn.push(value);
+    });
+}
+
+deps.next('index');
+
+setTimeout(function () {
+    console.log(globalTree);
+}, 1000);
