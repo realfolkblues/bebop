51b4fbb03979e97e0fc2f80d3ce11240b9c387f3
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2017-06-18 18:15:53 +0200

    Added acorn

--- src/experiments/riccardobevilacqua/analyzer.ts
+++ src/experiments/riccardobevilacqua/analyzer.ts
@@ -20,6 +20,9 @@
 import { readFile } from 'fs';
 import { resolve } from 'path';
 import { StringDecoder } from 'string_decoder';
+import * as estree from '@types/estree';
+import * as acorn from 'acorn';
+import * as acornTypes from '@types/acorn';
 import * as babylon from 'babylon';
 import * as babelTypes from 'babel-types';
 import { Observable, Subject, BehaviorSubject } from 'rxjs/Rx';
@@ -34,11 +37,55 @@ class Analyzer {
         this.encoding = encoding;
 
         this.files.subscribe(filename => {
-            const nodes = this.getAstFromFile(resolve(this.dirPath, filename) + '.js');
+            const fileFullPath = resolve(this.dirPath, filename) + '.js';
+            const acornAst = this.getAcornAstFromFile(fileFullPath);
+            const babylonAst = this.getBabylonAstFromFile(fileFullPath);
+
+            acornAst.subscribe({
+                next: (value: estree.Program) => {
+                    console.log('==== ACORN AST');
+                }
+            })
+
+            babylonAst.subscribe({
+                next: (value: babelTypes.File) => {
+                    console.log('==== BABYLON AST');
+                },
+                complete: () => console.log('Complete!')
+            })
         });
     }
 
-    getAstFromFile(filename: string): Observable<babelTypes.File> {
+    getAcornAstFromFile(filename: string): Observable<estree.Program> {
+        const readFileAsObservable = Observable.bindNodeCallback((
+            path: string,
+            encoding: string,
+            callback: (error: Error, buffer: Buffer) => void
+        ) => readFile(path, encoding, callback));
+
+        const tree = readFileAsObservable(filename, this.encoding)
+            .flatMap(buffer => {
+                const astAsObservable = Observable.bindNodeCallback((
+                    codeBuffer: Buffer,
+                    callback: (error: Error, tree: estree.Program) => void
+                ) => {
+                    const decoder = new StringDecoder(this.encoding);
+                    const ast = acorn.parse(decoder.write(codeBuffer), {
+                        allowImportExportEverywhere: true,
+                        sourceFile: filename,
+                        sourceType: 'module'
+                    });
+
+                    return ast;
+                });
+
+                return astAsObservable(buffer);
+            });
+
+        return tree;
+    }
+
+    getBabylonAstFromFile(filename: string): Observable<babelTypes.File> {
         const readFileAsObservable = Observable.bindNodeCallback((
             path: string,
             encoding: string,
