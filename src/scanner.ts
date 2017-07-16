686ca98141f2168c57faf357cdf8b2dab3a8b901
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2017-07-16 18:10:09 +0200

    Updated identifiers analysis

--- src/scanner.ts
+++ src/scanner.ts
@@ -16,9 +16,8 @@ export default class Scanner {
             next: (ast: babelTypes.File) => {
                 const astCollectionOriginal: jscodeshift.Collection = jscodeshift(ast);
                 const astModded = this.scanASTCollection(astCollectionOriginal).getAST();
-                // const astModded = this.scanInvokedFn(astCollectionOriginal).getAST();
 
-                // this.astStreamModded.next(astModded);
+                this.astStreamModded.next(astModded);
             }, 
             error: (err: Error) => {
                 console.error(err);
@@ -33,15 +32,37 @@ export default class Scanner {
         const identifiers: Subject<jscodeshift.Identifier> = new Subject<jscodeshift.Identifier>();
 
         identifiers.subscribe({
-            next: (nodePath) => {
-                const nodeName = nodePath.node.name;
-                const nodeScope = nodePath.scope;
-                const nodeParent = nodePath.parent;
+            next: (identifierNodePath) => {
+                const identifierName = identifierNodePath.node.name;
 
-
-                console.info('== NODE:', nodeName, ' PARENT ', nodeParent.node.type);
-                if (this.isDeclaration(nodeParent)) {
-                    console.log('PARENT IS DECLARATION');
+                if (!this.isDeclaration(identifierNodePath.parent)) {
+                    astCollection
+                        .find(jscodeshift.FunctionDeclaration, {
+                            id: {
+                                name: identifierName
+                            }
+                        })
+                        .forEach(nodePath => {
+                            if (parseInt(nodePath['references']) > -1) {
+                                nodePath.references++;
+                            } else {
+                                nodePath['references'] = 1;
+                            }
+                        });
+                        
+                    astCollection
+                        .find(jscodeshift.VariableDeclarator, {
+                            id: {
+                                name: identifierName
+                            }
+                        })
+                        .forEach(nodePath => {
+                            if (parseInt(nodePath['references']) > -1) {
+                                nodePath.references++;
+                            } else {
+                                nodePath['references'] = 1;
+                            }
+                        });    
                 }
             },
             error: (err: Error) => {
@@ -61,52 +82,7 @@ export default class Scanner {
         return astCollection;
     }
 
-    scanInvokedFn(astCollection: jscodeshift.Collection): jscodeshift.Collection {
-        const invokedFn: Subject<jscodeshift.CallExpression> = new Subject<jscodeshift.CallExpression>();
-
-        invokedFn.subscribe({
-            next: (value) => {
-                astCollection
-                    .find(jscodeshift.FunctionDeclaration, {
-                        id: {
-                            name: value
-                        }
-                    })
-                    .forEach(nodePath => {
-                        if (parseInt(nodePath['references']) > -1) {
-                            nodePath.references++;
-                        } else {
-                            nodePath['references'] = 1;
-                        }
-                    });
-            },
-            error: (err: Error) => {
-                console.error(err);
-            },
-            complete: () => {
-                console.log('Invoked functions stream completed');
-            }
-        });
-
-        const invokedFnCollection: jscodeshift.Collection = this.getInvokedFn(astCollection);
-
-        invokedFnCollection.forEach(nodePath => {
-            invokedFn.next(nodePath.value.callee.name); 
-        });
-
-        return astCollection;
-    }
-
-    getInvokedFn(astCollection: jscodeshift.Collection): jscodeshift.Collection {
-        return astCollection
-            .find(jscodeshift.CallExpression, {
-                callee: {
-                    type: 'Identifier'
-                }
-            });
-    }
-
-    getASTStream() {
+    getASTStream(): any {
         return this.astStreamModded;
     }
 
