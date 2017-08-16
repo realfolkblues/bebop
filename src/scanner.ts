957b558d409228485452c510f0082b0d40042dce
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2017-08-16 23:48:40 +0200

    Updated scanner (wip)

--- src/scanner.ts
+++ src/scanner.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path';
 import * as babelTypes from 'babel-types';
 import { Observable, Subject } from 'rxjs/Rx';
 import * as jscodeshift from 'jscodeshift';
@@ -10,18 +11,39 @@ export default class Scanner {
     constructor(crawler: Crawler) { 
         this.astStreamInput = crawler.getASTStream();
 
-        this.scanCrossStream();
+        this.start();
     }
 
-    scanCrossStream(): void {
-        this.scanASTStream();
+    /**
+     * Launch cross AST stream analysis
+     * 
+     * >Don't cross the streams.
+     * - Egon Spengler
+     */
+    start(): void {
+        const astStreamScanned: Observable<babelTypes.File> = this.scanASTStream();
         
-        this.scanASTStream().subscribe({
+        astStreamScanned.subscribe({
             next: (ast: babelTypes.File) => {
                 jscodeshift(ast)
-                    .find(jscodeshift.FunctionDeclaration)
+                    .find(jscodeshift.ImportDeclaration)
                     .forEach(nodePath => {
-                        console.info(nodePath.node.id.name, nodePath.references);
+                        const modulePath: string = resolve(nodePath.node.source.value + '.js');
+                        console.info('== MODULE PATH:', modulePath);
+                        const astStreamMirror: Observable<babelTypes.File> = this.astStreamInput
+                            .repeat(1)
+                            .filter(astMirror => {
+                                const fileName: string = astMirror[0].value.loc.SourceLocation.filename;
+                                console.info('== FILENAME:', fileName);
+                                return modulePath === fileName;
+                            });
+
+                        astStreamMirror.subscribe({
+                            next: (astCross: babelTypes.File) => {
+                                console.info('== CHECK FILENAME', astCross[0].value.loc.SourceLocation.filename);
+                            }
+                        });
+
                     });
             },
             error: (err: Error) => {
@@ -35,6 +57,7 @@ export default class Scanner {
 
     /**
      * Launch AST stream analysis
+     * @param astStream
      */
     scanASTStream(astStream: Observable<babelTypes.File> = this.astStreamInput): Subject<babelTypes.File> {
         const astStreamOutput: Subject<babelTypes.File> = new Subject<babelTypes.File>();
