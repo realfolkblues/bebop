a7128f8e109b0bece0f266940fab817c8621628a
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2017-08-25 16:52:38 +0200

    Added monitor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 
+import Monitor from './monitor';
 import Crawler from './crawler';
 import Scanner from './scanner';
 import Epurator from './epurator';
@@ -6,6 +7,7 @@ import Epurator from './epurator';
 const cwd = process.cwd();
 const targetPath = process.env.npm_config_target_path || './examples/fn/01/index.js';
 const crawler = new Crawler(targetPath);
+const monitor = new Monitor(crawler);
 const scanner = new Scanner(crawler);
 
 crawler.start();
---
+++ src/monitor.ts
@@ -0,0 +1,8 @@
+import Crawler from './crawler';
+
+export default class Monitor { 
+    
+    constructor(craweler: Crawler) {
+        
+    }
+}
