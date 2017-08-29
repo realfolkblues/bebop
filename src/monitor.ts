7d99e53e6cf11e57cbb0bf0861a9ca9be11d9983
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2017-08-29 17:19:27 +0200

    Updated monitor

--- src/monitor.ts
+++ src/monitor.ts
@@ -1,11 +1,14 @@
-import { resolve, dirname } from 'path';
-import { Observable } from 'rxjs/Rx';
+import { BehaviorSubject, Observable } from 'rxjs/Rx';
 import Crawler from './crawler';
-import { IResolverModule } from './resolver';
+
+export interface IMonitorModule {
+    fullPath: string,
+    processed: boolean
+}
 
 export default class Monitor { 
-    fileStack: string[] = []
     fileStream: Observable<string>
+    stackStream: BehaviorSubject<IMonitorModule[]> = new BehaviorSubject([])
     
     constructor(crawler: Crawler) {
         this.fileStream = crawler.discoverFiles();
@@ -13,10 +16,38 @@ export default class Monitor {
         this.start();
     }
 
+    addMonitorModule(fullPath: string): IMonitorModule[] {
+        const snapshot = this.stackStream.getValue();
+
+        snapshot.push(<IMonitorModule>{
+            fullPath,
+            processed: false
+        });
+
+        this.stackStream.next(snapshot);
+
+        return this.stackStream.getValue();
+    }
+
     start(): void {
+        this.stackStream.subscribe({
+            next: (stack: IMonitorModule[]) => {
+                console.info('Monitor stack', stack);
+            },
+            error: (err: Error) => {
+                console.error(err);
+            }
+        });
+
         this.fileStream.subscribe({
             next: (fullPath: string) => {
-                console.log('Monitor intercepted [' + fullPath + ']');
+                this.addMonitorModule(fullPath);
+            },
+            error: (err: Error) => {
+                console.error(err);
+            },
+            complete: () => {
+                console.log('Monitor file stream completed');
             }
         });
     }
