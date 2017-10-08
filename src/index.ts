f0e67e1bab6e1bf146ffce4596732667659595b3
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2017-10-08 16:40:24 +0200

    Removed monitor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 
-import Monitor from './monitor';
 import Crawler from './crawler';
 import Scanner from './scanner';
 import Epurator from './epurator';
@@ -7,7 +6,6 @@ import Epurator from './epurator';
 const cwd = process.cwd();
 const targetPath = process.env.npm_config_target_path || './examples/fn/01/index.js';
 const crawler = new Crawler(targetPath);
-const monitor = new Monitor(crawler);
 const scanner = new Scanner(crawler);
 
 crawler.start();
