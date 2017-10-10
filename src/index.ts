950c82e5df9b0f5c2979797fa3eb349a91d47990
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2017-10-11 00:27:35 +0200

    Added log messages

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import Crawler from './crawler';
 import Scanner from './scanner';
 import Epurator from './epurator';
 
+console.log('== BEGIN ============================================================');
 const cwd = process.cwd();
 const targetPath = process.env.npm_config_target_path || './examples/fn/01/index.js';
 const crawler = new Crawler(targetPath);
 const scanner = new Scanner(crawler);
 
 crawler.start();
+console.log('== END ==============================================================');
