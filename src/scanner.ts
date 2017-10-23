01033c5d0cbb224e3b47ffe1490300d004632b39
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2017-10-23 02:06:39 +0200

    Updated scanner (wip)

--- src/scanner.ts
+++ src/scanner.ts
@@ -22,54 +22,54 @@ export default class Scanner {
         return this.stack.find((astModule: IASTModule) => astModule.fullPath === fullPath);
     }
 
-    scanImport(): void {
-        this.astListStream.subscribe({
-            next: ((astList: babelTypes.File[]) => {
-                if (astList && astList.length > 1) {
-                    console.info('Processing list of ' + astList.length + ' AST...');
-                    const listMonitor: Observable<babelTypes.File> = Observable.from(astList);
+    // scanImport(): void {
+    //     this.astListStream.subscribe({
+    //         next: ((astList: babelTypes.File[]) => {
+    //             if (astList && astList.length > 1) {
+    //                 console.info('Processing list of ' + astList.length + ' AST...');
+    //                 const listMonitor: Observable<babelTypes.File> = Observable.from(astList);
 
-                    listMonitor.subscribe({
-                        next: (ast: babelTypes.File) => {
-                            jscodeshift(ast)
-                                .find(jscodeshift.ImportDeclaration)
-                                .forEach(nodePath => {
-                                    const importedModuleFullPath: string = resolve(this.crawler.sourceDir, nodePath.node.source.value + '.js');
-                                    console.info('Found imported module [' + importedModuleFullPath + ']');
+    //                 listMonitor.subscribe({
+    //                     next: (ast: babelTypes.File) => {
+    //                         jscodeshift(ast)
+    //                             .find(jscodeshift.ImportDeclaration)
+    //                             .forEach(nodePath => {
+    //                                 const importedModuleFullPath: string = resolve(this.crawler.sourceDir, nodePath.node.source.value + '.js');
+    //                                 console.info('Found imported module [' + importedModuleFullPath + ']');
 
-                                    const importedModuleAst: babelTypes.File = astList
-                                        .find((moduleAst: babelTypes.File) => {
-                                            return moduleAst[0].value.program.loc.filename === importedModuleFullPath;
-                                        });
+    //                                 const importedModuleAst: babelTypes.File = astList
+    //                                     .find((moduleAst: babelTypes.File) => {
+    //                                         return moduleAst[0].value.program.loc.filename === importedModuleFullPath;
+    //                                     });
 
-                                    this.scanDeclaration(importedModuleAst, importedModuleFullPath);
-                                });
-                        },
-                        error: (err: Error) => {
-                            console.error(err);
-                        },
-                        complete: () => {
-                            console.log('List monitor stream completed');
-                        }
-                    });
-                }
-            }),
-            error: (err: Error) => {
-                console.error(err);
-            },
-            complete: () => {
-                console.log('Scanning cross module: level 2 completed');
-            }
-        });
-    }
+    //                                 this.scanDeclaration(importedModuleAst, importedModuleFullPath);
+    //                             });
+    //                     },
+    //                     error: (err: Error) => {
+    //                         console.error(err);
+    //                     },
+    //                     complete: () => {
+    //                         console.log('List monitor stream completed');
+    //                     }
+    //                 });
+    //             }
+    //         }),
+    //         error: (err: Error) => {
+    //             console.error(err);
+    //         },
+    //         complete: () => {
+    //             console.log('Scanning cross module: level 2 completed');
+    //         }
+    //     });
+    // }
 
     /**
      * Add number of references to declarations in a given AST
      * @param ast
      * @param identifierName
      */
-    scanDeclaration(ast: babelTypes.File, identifierName: string = ''): babelTypes.File {
-        const astCollection: jscodeshift.Collection = jscodeshift(ast);
+    scanDeclaration(astModule: IASTModule, identifierName: string = ''): IASTModule {
+        const astCollection: jscodeshift.Collection = jscodeshift(astModule.ast);
         const identifiers: Observable<jscodeshift.Identifier> = Observable.from(astCollection.find(jscodeshift.Identifier).__paths);
 
         identifiers.subscribe({
@@ -107,31 +107,9 @@ export default class Scanner {
                 console.log('Identifiers stream completed');
             }
         });
-
-        return astCollection.getAST();
-    }
-
-    setupASTListStream(inputStream: Observable<babelTypes.File> = this.inputStream): void {
-        const astStreamScanned: Observable<babelTypes.File> = Observable
-            .from(inputStream)
-            .map((ast: babelTypes.File) => {
-                return this.scanDeclaration(ast);
-            });
         
-        astStreamScanned.subscribe({
-            next: (ast: babelTypes.File) => {
-                const astList = this.astListStream.getValue();
-
-                astList.push(ast);
-                this.astListStream.next(astList);
-            },
-            error: (err: Error) => {
-                console.error(err);
-            },
-            complete: () => {
-                console.log('Scanning cross module: level 1 completed');
-            }
-        });
+        astModule.ast = astCollection.getAST();
+        return astModule;
     }
 
     start(): void {
@@ -143,11 +121,7 @@ export default class Scanner {
             next: (astModule: IASTModule) => {
                 console.log('Scanner received [' + astModule.fullPath + ']');
                 this.stack.push(astModule);
-                console.info('Stack', this.stack);
             }
         });
-
-        // this.setupASTListStream();
-        // this.scanImport();
     }
 }
