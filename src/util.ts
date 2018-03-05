4a54275ca7e6464b1267ef9482b219a00a6b831c
Riccardo Bevilacqua <riccardo.bevilacqua@morningstar.com>
2018-03-05 18:20:49 +0100

    Applied estraverse to visit ast

--- src/util.ts
+++ src/util.ts
@@ -1,74 +1,18 @@
 import * as estree from 'estree';
-import * as recast from 'recast';
-import * as jscodeshift from 'jscodeshift';
-import { concat, flatten } from 'ramda';
+import { traverse } from 'estraverse';
 
-// Recast
-export function visitAST(ast: estree.Program, nodeType: string = '', cb: Function, deep: boolean = false): estree.Program {
+export function visitAST(ast: estree.Program, nodeType: string = '', cb: Function): estree.Program {
     let visitor: Object = {};
 
     if (nodeType.length > 0 && cb && typeof cb === 'function') {
-        const visitorMethodName: string = 'visit' + nodeType;
-
-        visitor[visitorMethodName] = (nodePath) => {
-            cb(nodePath);
-
-            if (!deep) {
-                return false;
+        traverse(ast, {
+            enter: (node, parent) => {
+                if (node.type === nodeType) {
+                    cb(node, parent);
+                }
             }
-            
-            this.traverse(nodePath);
-        };
-
-        recast.visit(ast, visitor);
+        });
     }
 
     return ast;
-}
-
-// JSCodeShift
-function functionDeclarationsFilter(call, caller) {
-    if (call.value.callee && caller.value.id) {
-        return call.value.callee.name === caller.value.id.name;
-    }
-
-    return false;
-}
-
-function statementDeclarationsFilter(statement, caller) {
-    if (statement.value.argument && caller.value.id) {
-        return statement.value.argument.name === caller.value.id.name;
-    }
-
-    return false;
-}
-
-const utils = {
-    markFunctions: function (): void {
-        this
-            .find(jscodeshift.CallExpression)
-            .first('Function', functionDeclarationsFilter)
-            .mark();
-
-        this
-            .find(jscodeshift.ReturnStatement)
-            .first('Function', statementDeclarationsFilter)
-            .mark();
-
-        this
-            .find(jscodeshift.ExportNamedDeclaration)
-            .find(jscodeshift.Function)
-            .mark();
-    },
-
-    shake: function (): string {
-        return this
-            .find(jscodeshift.FunctionDeclaration)
-            .filter(path => !path.keep)
-            .remove();
-    }
-}
-
-export default function registerUtils(): void {
-    jscodeshift.registerMethods(utils);
 }
